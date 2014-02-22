003de5441667f32a14f32e4a703f42f8d48f0789
Dave Vinzant <trizzant@gmail.com>
2014-02-21 18:51:21 -0800

    undo

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-48328417-1'
+
 module Pinteresting
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
